fbd2be0e30ab1938686f17745f63214826b480a5
Lim Kar Joon <djzzlim@gmail.com>
2025-03-23 15:08:22 +0800

    Modified existing post - Why I chose Valaxy

--- .valaxy/typed-router.d.ts
+++ .valaxy/typed-router.d.ts
@@ -28,7 +28,7 @@ declare module 'vue-router/auto-routes' {
     '/links/': RouteRecordInfo<'/links/', '/links', Record<never, never>, Record<never, never>>,
     '/page/[page]': RouteRecordInfo<'/page/[page]', '/page/:page', { page: ParamValue<true> }, { page: ParamValue<false> }>,
     '/posts/': RouteRecordInfo<'/posts/', '/posts', Record<never, never>, Record<never, never>>,
-    '/posts/hello-valaxy': RouteRecordInfo<'/posts/hello-valaxy', '/posts/hello-valaxy', Record<never, never>, Record<never, never>>,
+    '/posts/2025-03-23-why-i-chose-valaxy': RouteRecordInfo<'/posts/2025-03-23-why-i-chose-valaxy', '/posts/2025-03-23-why-i-chose-valaxy', Record<never, never>, Record<never, never>>,
     '/tags/': RouteRecordInfo<'/tags/', '/tags', Record<never, never>, Record<never, never>>,
   }
 }
