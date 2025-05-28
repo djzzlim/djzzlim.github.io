ef6dfcb61e0b17cc9507fa926e96d1ee51661e65
djzzlim <djzzlim@users.noreply.github.com>
2025-05-28 07:21:28 +0000

    deploy: 564605bf3268a98693f5a091b051177354993437

---
+++ assets/back.BcfdMSuk.js
@@ -0,0 +1,1 @@
+import{u as o}from"./chunks/vue-router.qUYA7ODc.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
